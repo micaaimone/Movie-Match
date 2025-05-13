ac968157a7fe0aa4a18c983356ca3c52b47b34ff
Lautaro Araya <lau9araya@gmail.com>
2025-05-13 14:12:18 -0300

    nvd

---
+++ src/main/java/com/example/ProyectoMovieMatch/model/entities/suscripcion/PlanSuscripcionEntity.java
@@ -0,0 +1,18 @@
+package com.example.ProyectoMovieMatch.model.entities.suscripcion;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "plan_suscripcion")
+public class PlanSuscripcionEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private TipoSuscripcion tipo;
+
+    private float precio;
+}
