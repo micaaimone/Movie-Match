d186cc62d584db9a70069d6291c6708bcb11787b
Micaela Morano <bbrisamorano@gmail.com>
2025-05-13 10:28:59 -0300

    creacion de usuarioEntity junto con sus beans y credencialEntity

--- src/main/java/com/example/ProyectoMovieMatch/model/entities/CredencialEntity.java
+++ src/main/java/com/example/ProyectoMovieMatch/model/entities/CredencialEntity.java
@@ -1,5 +1,6 @@
 package com.example.ProyectoMovieMatch.model.entities;
 
+import com.example.ProyectoMovieMatch.model.enums.E_Rango;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
